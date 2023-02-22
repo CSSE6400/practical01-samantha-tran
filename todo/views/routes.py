3281bebb7e8d3eda90ba21022be5814b0f8aa719
Samantha Tran <stran@Samanthas-MacBook-Air.local>
2023-02-22 11:36:56 +1000

    finished prac 1

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -29,3 +29,39 @@ def get_todo(id):
     "created_at": "2023-02-20T00:00:00",
     "updated_at": "2023-02-20T00:00:00"
     })
+
+@api.route('/todos', methods=['POST'])
+def create_todo():
+    return jsonify({
+    "id": 1,
+    "title": "Watch CSSE6400 Lecture",
+    "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+    "completed": False,
+    "deadline_at": "2023-02-27T00:00:00",
+    "created_at": "2023-02-20T00:00:00",
+    "updated_at": "2023-02-20T00:00:00"
+    })
+
+@api.route('/todos/<int:id>', methods=['PUT'])
+def update_todo(id):
+    return jsonify({
+    "id": id,
+    "title": "Watch CSSE6400 Lecture!!",
+    "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+    "completed": False,
+    "deadline_at": "2023-02-27T00:00:00",
+    "created_at": "2023-02-20T00:00:00",
+    "updated_at": "2023-02-20T00:00:00"
+    })
+
+@api.route('/todos/<int:id>', methods=['DELETE'])
+def delete_todo(id):
+    return jsonify({
+    "id": id,
+    "title": "Watch CSSE6400 Lecture",
+    "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+    "completed": False,
+    "deadline_at": "2023-02-27T00:00:00",
+    "created_at": "2023-02-20T00:00:00",
+    "updated_at": "2023-02-20T00:00:00"
+    })
